8a01b114f3a0eee14dff0dda90889c7eb30eca7a
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2021-12-05 12:54:29 +0800

    Javascript basic

---
+++ Code/Javascript/Truthy_Falsy/app.js
@@ -0,0 +1,19 @@
+const userInput = prompt("Please enter something");
+
+if (userInput) {
+    console.log("TRUTHY!")
+} else {
+    console.log("FALSY!")
+}
+
+// if (0) {
+//     console.log("TRUTHY")
+// } else {
+//     console.log("FALSY")
+// }
+
+// if (' ') {
+//     console.log("TRUTHY")
+// } else {
+//     console.log("FALSY")
+// }
