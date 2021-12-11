4e5c68b57063a4978fbcbb373bc8fa01ec795dec
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2021-12-12 02:30:18 +0800

    TV Show Search

--- Code/AJAX/TVShowSearch/app.js
+++ Code/AJAX/TVShowSearch/app.js
@@ -1,9 +1,9 @@
-const form = document.querySelector('#searchForm');
+const form = document.querySelector('#searchForm')
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config)
     makeImages(res.data)
     form.elements.query.value = '';
 })
