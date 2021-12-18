523b31b2d01deb8390db57293a118d242b6739a9
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2021-12-18 14:53:14 +0800

    Update index.js

--- Code/Middleware_Intro/index.js
+++ Code/Middleware_Intro/index.js
@@ -2,14 +2,53 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'))
+app.use(morgan('tiny'))
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path)
+    next();
+})
+
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
+})
+
+const verifyPassword = ((req, res, next) => {
+    const { password } = req.query;
+    if (password === 'chickennugget') {
+        next();
+    }
+    res.send('SORRY YOU NEED A PASSWORD!!!')
+})
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!")
+//     return next();
+// })
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE!!!")
+//     return next();
+// })
+
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE ${req.requestTime}`)
     res.send('home page')
 })
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE ${req.requestTime}`)
     res.send('I AM A DOG WOOF')
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
+})
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
 })
 
 app.listen(3000, () => {
